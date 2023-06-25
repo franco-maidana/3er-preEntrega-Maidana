905238451a13e9c6d198a389e07648c1dec71236
Franco Maidana <francomaidana094@gmail.com>
2023-06-25 14:35:34 -0300

    V 0.0.3
    se agrego codigo de filtrado por productos y de agregaron funciones y se habilito con el dom los botones de navegacion de congelados almacen bebidad y bazar

--- codigos.js
+++ codigos.js
@@ -31,10 +31,131 @@ for (const producto of productosAComprar) {
 
 // filtrados
 
-let ver = productosAComprar.filter((productosAComprar) => {
-  return productosAComprar.categoria;
-})
-console.log(ver);
+// funcion para mostrar el filtrado y los productos 
+function mostrarProductosBazar() {
+  
+  // borrar cualquier producto existente 
+  productosMostrados.innerHTML = '';
 
-let bazar = document.getElementById('Bazar');
-console.log(bazar);
+  // filtramos el producto por categoria 'Bazar'
+  let productosFiltrados = productosAComprar.filter((producto) => {
+    return producto.categoria === 'Bazar';
+  });
+
+  // creamos y mostramos tarjeta de productos para los  productos filtrados bazar
+  for (const producto of productosFiltrados) {
+    let carta = document.createElement('div');
+    carta.className = 'hola card col-md-4';
+    carta.innerHTML = `
+      <div class="chau card">
+        <img class="card-img-top" src="${producto.imagen}" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">${producto.nombre}</h5>
+          <h6 class="card-title">${producto.marca}</h6>
+          <p class="card-text">${producto.precio}</p>
+          <button class="btn btn-primary">Comprar</button>
+        </div>
+      </div>
+    `;
+    productosMostrados.appendChild(carta);
+  };
+}
+
+// agarramos el elemento del boton 
+let bazarButton = document.getElementById('Bazar');
+
+// agregamos el evento al boton
+bazarButton.addEventListener('click', mostrarProductosBazar);
+
+
+// funcion para mostrar el filtrado y los productos Almecen
+function mostrarProductosAlmacen() {
+  productosMostrados.innerHTML = '';
+
+  let productosAlmacen = productosAComprar.filter((producto) => {
+    return producto.categoria === 'Almacen';
+  });
+
+  // creamos y mostramos tarjeta de productos para los  productos filtrados bazar
+  for (const producto of productosAlmacen) {
+    let carta = document.createElement('div');
+    carta.className = 'hola card col-md-4';
+    carta.innerHTML = `
+      <div class="chau card">
+        <img class="card-img-top" src="${producto.imagen}" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">${producto.nombre}</h5>
+          <h6 class="card-title">${producto.marca}</h6>
+          <p class="card-text">${producto.precio}</p>
+          <button class="btn btn-primary">Comprar</button>
+        </div>
+      </div>
+    `;
+    productosMostrados.appendChild(carta);
+  }
+}
+
+let almacenButton = document.getElementById('Almacen');
+
+almacenButton.addEventListener('click', mostrarProductosAlmacen);
+
+
+
+function mostrarProductosBebidas() {
+  productosMostrados.innerHTML = '';
+
+  let productosBebidas = productosAComprar.filter((producto) => {
+    return producto.categoria === 'Bebidas';
+  });
+
+  for (const producto of productosBebidas) {
+    let carta = document.createElement('div');
+    carta.className = 'hola card col-md-4';
+    carta.innerHTML = `
+      <div class="chau card">
+        <img class="card-img-top" src="${producto.imagen}" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">${producto.nombre}</h5>
+          <h6 class="card-title">${producto.marca}</h6>
+          <p class="card-text">${producto.precio}</p>
+          <button class="btn btn-primary">Comprar</button>
+        </div>
+      </div>
+    `;
+    productosMostrados.appendChild(carta);
+  }
+}
+
+let bebidasButton = document.getElementById('Bebidas');
+
+bebidasButton.addEventListener('click', mostrarProductosBebidas);
+
+
+function mostrarProductosCongelados() {
+  productosMostrados.innerHTML = '';
+
+  let productosBebidas = productosAComprar.filter((producto) => {
+    return producto.categoria === 'Congelados';
+  });
+
+  for (const producto of productosBebidas) {
+    let carta = document.createElement('div');
+    carta.className = 'hola card col-md-4';
+    carta.innerHTML = `
+      <div class="chau card">
+        <img class="card-img-top" src="${producto.imagen}" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">${producto.nombre}</h5>
+          <h6 class="card-title">${producto.marca}</h6>
+          <p class="card-text">${producto.precio}</p>
+          <button class="btn btn-primary">Comprar</button>
+        </div>
+      </div>
+    `;
+    productosMostrados.appendChild(carta);
+  }
+}
+
+let congeladosBotton = document.getElementById('Congelados');
+
+congeladosBotton.addEventListener('click', mostrarProductosCongelados);
