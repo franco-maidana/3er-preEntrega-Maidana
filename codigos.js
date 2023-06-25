3379d75f0839fdc0eebc98aa5962657c12937750
Franco Maidana <francomaidana094@gmail.com>
2023-06-25 09:49:07 -0300

    V 0.0.2
    agregando cartas de productos y filtrados

--- codigos.js
+++ codigos.js
@@ -8,3 +8,33 @@ burger.addEventListener('click', () => {
     nav.style.display = 'block';
   }
 });
+
+// cartas 
+
+let productosMostrados = document.getElementById('cartas');
+
+for (const producto of productosAComprar) {
+  let carta = document.createElement('div')
+  carta.className = 'hola card col-md-4';
+  carta.innerHTML = `
+    <div class="chau card">
+    <img class="card-img-top" src="${producto.imagen}" alt="Card image cap">
+    <div class="card-body">
+          <h5 class="card-title">${producto.nombre}</h5>
+          <h6 class="card-title">${producto.marca}</h6>
+          <p class="card-text">${producto.precio}</p>
+          <buton class="btn btn-primary">Comprar</button>
+    </div>
+    `;
+  productosMostrados.appendChild(carta);
+}
+
+// filtrados
+
+let ver = productosAComprar.filter((productosAComprar) => {
+  return productosAComprar.categoria;
+})
+console.log(ver);
+
+let bazar = document.getElementById('Bazar');
+console.log(bazar);
