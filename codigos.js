9a16fd84e1bfff59663e7abfe8b0cf9b770a2ec6
Franco Maidana <francomaidana094@gmail.com>
2023-06-25 16:54:09 -0300

    V 0.0.4
    agregando filtrado y para que se vea en pantalla  las siguientes categorias fiambres verdura limpieza panaderia

--- codigos.js
+++ codigos.js
@@ -130,6 +130,7 @@ let bebidasButton = document.getElementById('Bebidas');
 
 bebidasButton.addEventListener('click', mostrarProductosBebidas);
 
+// productos congelados
 
 function mostrarProductosCongelados() {
   productosMostrados.innerHTML = '';
@@ -159,3 +160,128 @@ function mostrarProductosCongelados() {
 let congeladosBotton = document.getElementById('Congelados');
 
 congeladosBotton.addEventListener('click', mostrarProductosCongelados);
+
+// fiambres 
+
+
+function mostrarProductosFiabres() {
+  productosMostrados.innerHTML = '';
+
+  let productosBebidas = productosAComprar.filter((producto) => {
+    return producto.categoria === 'Fiambrería';
+  });
+
+  for (const producto of productosBebidas) {
+    let carta = document.createElement('div');
+    carta.className = 'hola card col-md-4';
+    carta.innerHTML = `
+      <div class="chau card">
+        <img class="card-img-top" src="${producto.imagen}" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">${producto.nombre}</h5>
+          <h6 class="card-title">${producto.marca}</h6>
+          <p class="card-text">${producto.precio}</p>
+          <button class="btn btn-primary">Comprar</button>
+        </div>
+      </div>
+    `;
+    productosMostrados.appendChild(carta);
+  }
+}
+
+let fiambresBotton = document.getElementById('Fiambreria');
+
+fiambresBotton.addEventListener('click', mostrarProductosFiabres);
+
+// Frutas y verdudas
+
+function mostrarProductosVerduleria() {
+  productosMostrados.innerHTML = '';
+
+  let productosVerduras = productosAComprar.filter((producto) => {
+    return producto.categoria === 'FrutasYVerduras';
+  });
+
+  for (const producto of productosVerduras) {
+    let carta = document.createElement('div');
+    carta.className = 'hola card col-md-4';
+    carta.innerHTML = `
+      <div class="chau card">
+        <img class="card-img-top" src="${producto.imagen}" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">${producto.nombre}</h5>
+          <h6 class="card-title">${producto.marca}</h6>
+          <p class="card-text">${producto.precio}</p>
+          <button class="btn btn-primary">Comprar</button>
+        </div>
+      </div>
+    `;
+    productosMostrados.appendChild(carta);
+  }
+}
+
+let VerdurasBotton = document.getElementById('FrutasYVerduras');
+
+VerdurasBotton.addEventListener('click', mostrarProductosVerduleria);
+
+// liempieza 
+
+function mostrarProductosLimpieza() {
+  productosMostrados.innerHTML = '';
+
+  let productosLimpieza = productosAComprar.filter((producto) => {
+    return producto.categoria === 'Limpieza';
+  });
+
+  for (const producto of productosLimpieza) {
+    let carta = document.createElement('div');
+    carta.className = 'hola card col-md-4';
+    carta.innerHTML = `
+      <div class="chau card">
+        <img class="card-img-top" src="${producto.imagen}" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">${producto.nombre}</h5>
+          <h6 class="card-title">${producto.marca}</h6>
+          <p class="card-text">${producto.precio}</p>
+          <button class="btn btn-primary">Comprar</button>
+        </div>
+      </div>
+    `;
+    productosMostrados.appendChild(carta);
+  }
+}
+
+let liempiezaBotton = document.getElementById('Limpieza');
+
+liempiezaBotton.addEventListener('click', mostrarProductosLimpieza);
+
+// panaderia 
+
+function mostrarProductosPanaderia() {
+  productosMostrados.innerHTML = '';
+
+  let productosPanaderia = productosAComprar.filter((producto) => {
+    return producto.categoria === 'Panadería';
+  });
+
+  for (const producto of productosPanaderia) {
+    let carta = document.createElement('div');
+    carta.className = 'hola card col-md-4';
+    carta.innerHTML = `
+      <div class="chau card">
+        <img class="card-img-top" src="${producto.imagen}" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">${producto.nombre}</h5>
+          <h6 class="card-title">${producto.marca}</h6>
+          <p class="card-text">${producto.precio}</p>
+          <button class="btn btn-primary">Comprar</button>
+        </div>
+      </div>
+    `;
+    productosMostrados.appendChild(carta);
+  }
+}
+
+let panaderiaBotton = document.getElementById('Panaderia');
+
+panaderiaBotton.addEventListener('click', mostrarProductosPanaderia);
